ccfc142dacc848942f8fbeefcc969e6fbd2dd894
elib0 <elijose.c@gmail.com>
2020-12-09 16:22:50 -0400

    Interiorist

---
+++ js/chunk-2d0cfc83.c120aded.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cfc83"],{"64d3":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an our work page")])])}],a=t("2877"),r={},u=Object(a["a"])(r,c,s,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0cfc83.c120aded.js.map
